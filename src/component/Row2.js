3bf8a4035a95e6edb41086f2ee5e48c78625d737
naman bansal <naman.namo1996@gmail.com>
2024-02-07 00:51:56 +0530

    graph added

--- src/component/Row2.js
+++ src/component/Row2.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './row2.css';
+import Chart from "react-apexcharts";
 
 export default function Row2() {
+  const[state,setState]= useState({
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ]
+  }
+  )
   return (
       <div className='row2'>
         <div className='widget1'>
@@ -124,7 +142,14 @@ export default function Row2() {
                     <option value="option3">Monthly</option>
                     <option value="option3">Daily</option>
                 </select></div>
+                <Chart
+              options={state.options}
+              series={state.series}
+              type="bar"
+              width="500"
+            />
               </div>
+              
           </div>
           <div className='frame1'>
             <div className='month'>
--- src/component/Row3.js
+++ src/component/Row3.js
@@ -4,7 +4,15 @@ import './row3.css';
 export default function Row3() {
   return (
     <div className='row3'>
-      <div className='chats'></div>
+      <div className='chats'>
+        <div className='heading'>
+          <h2>Chats</h2>
+          <p>2 unread messages</p>
+        </div>
+        <div className='customers'>
+          
+        </div>
+      </div>
       <div className='states'></div>
       <div className='deals'></div>
       
